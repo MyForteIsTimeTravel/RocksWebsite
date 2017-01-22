f615b7da8c7419f32377e006e539495e961aebdb
MyForteIsTimeTravel <RyanNeedham@outlook.com>
2017-01-22 16:38:39 +0000

    fixed some spacing issues
    '

--- js/input.js
+++ js/input.js
@@ -7,12 +7,12 @@ var middleX = window.innerWidth / 2
 var middleY = window.innerHeight / 2
 
 
-canvas.addEventListener("mouseover", function(e) {
-    onCanvas = true
-})
+canvas.addEventListener("mouseover", function (e) {onCanvas = true})
+canvas.addEventListener("mouseout",  function (e) {onCanvas = false})
 
-canvas.addEventListener("mouseout", function (e) {
-    onCanvas = false
+canvas.addEventListener("touchmove", function (e) {
+    mouseX = (event.clientX - middleX)
+    mouseY = (event.clientY - middleY)   
 })
 
 document.onmousemove = function (event) {
@@ -20,6 +20,10 @@ document.onmousemove = function (event) {
          mouseX = (event.clientX - middleX)
          mouseY = (event.clientY - middleY)   
     }
+}
+
+window.ondevicemotion = function (event) {
+    mouseX + event.accelerationIncludingGravity.x
 }
 
 function updateInput () {
