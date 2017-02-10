f862fe9e641ce968fae5dadff3bd670e571cbcdc
Ryan Needham <RyanNeedham@Outlook.com>
2017-02-10 20:55:21 +0000

    scrolling landscape, + some bugs

--- js/baked.js
+++ js/baked.js
@@ -1,8 +1,66 @@
+/* * * * * * * * * * * * * * * * * *
+ *  REPLICATE THIS NOISE MESH THING IN WEBGL
+ *
+ *    int cols, rows;
+ *    int scl = 20;
+ *    int w = 2000;
+ *    int h = 1600;
+
+    float flying = 0;
+
+    float[][] terrain;
+
+    void setup() {
+      size(600, 600, P3D);
+      cols = w / scl;
+      rows = h/ scl;
+      terrain = new float[cols][rows];
+    }
+
+
+    void draw() {
+
+      flying -= 0.1;
+
+      float yoff = flying;
+      for (int y = 0; y < rows; y++) {
+        float xoff = 0;
+        for (int x = 0; x < cols; x++) {
+          terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
+          xoff += 0.2;
+        }
+        yoff += 0.2;
+      }
+
+
+
+      background(23);
+      stroke(64);
+      noFill();
+
+      translate(width/2, height/2+50);
+      rotateX(PI/3);
+      translate(-w/2, -h/2);
+      for (int y = 0; y < rows-1; y++) {
+        beginShape(TRIANGLE_STRIP);
+        for (int x = 0; x < cols; x++) {
+          vertex(x*scl, y*scl, terrain[x][y]);
+          vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
+          //rect(x*scl, y*scl, scl, scl);
+        }
+        endShape();
+      }
+    }
+ *
+ * * * * * * * * * * * * * * * * * */
 window.onload = heroAnimation;
 
 function heroAnimation () {
+    
+    /* * * * * * * * * * * * * * * * * * 
+     * CONTEXT
+     * * * * * * * * * * * * * * * * * */
     var canvas = document.getElementById("heroCanvas")
-
     var gl = canvas.getContext('webgl')
     gl.clearColor(0.32, 0.32, 0.32, 1.0)
     gl.clear(gl.COLOR_BUFFER_BIT)
@@ -11,17 +69,22 @@ function heroAnimation () {
     gl.frontFace(gl.CCW)
     gl.cullFace(gl.BACK)
 
+    /* * * * * * * * * * * * * * * * * * 
+     * SHADER
+     * * * * * * * * * * * * * * * * * */
     var vertexShader = gl.createShader(gl.VERTEX_SHADER)
     gl.shaderSource(vertexShader, [
         'attribute vec3 vertPos;',
-        'attribute vec3 vertCol;',
+        //'attribute vec3 vertCol;',
         'varying vec3 fragCol;',
         'uniform mat4 model;',
         'uniform mat4 view;',
         'uniform mat4 projection;',
+        'float rand(vec2 co) { return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);}',
         'void main (void) {',
-            'fragCol = vertCol;',
-            'gl_Position = projection * view * model * vec4(vertPos, 1.0);',
+            //'fragCol = vertCol;',
+            'float lift = rand(vec2(vertPos.x, vertPos.z));',
+            'gl_Position = projection * view * model * vec4(vertPos.x, vertPos.y + lift, vertPos.z, 1.0);',
         '}'
     ].join('\n'))
     gl.compileShader(vertexShader)
@@ -32,116 +95,121 @@ function heroAnimation () {
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
     gl.shaderSource(fragmentShader, [
         'precision highp float;',
-        'varying vec3 fragCol;',
+        //'varying vec3 fragCol;',
         'void main (void) {',
-            'gl_FragColor = vec4(fragCol, 1.0);',
+            'gl_FragColor = vec4(0.21, 0.21, 0.21, 1.0);',
         '}'
     ].join('\n'))
     gl.compileShader(fragmentShader)
-        if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-            console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
-        }
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+    }
 
     var Shader = gl.createProgram()
     gl.attachShader(Shader, vertexShader)
     gl.attachShader(Shader, fragmentShader)
     gl.linkProgram(Shader)
 
-    var triVertices = new Float32Array ([
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0,
-        0.0, 0.5, 0.0
-    ])
+    /* * * * * * * * * * * * * * * * * * 
+     * MESH
+     * * * * * * * * * * * * * * * * * */
+    var width = 100;
+    var height = 100;
+    var startX = (width / 2) * -1;
+    var endZ = (height / 2) * -1;
+    
+    var xIndex = 0;
+    var zIndex = height;
+    
+    var landscape = [];
+    
+    for (var x = startX; x < width; x += 1.0, xIndex++) {
+        for (var z = height; z > endZ; z -= 1.0, zIndex--) {
+            // TRIANGLE 1 VERT 1
+            landscape.push(x - 0.5);    // position x
+            landscape.push(0.0);       // position y
+            landscape.push(z + 0.5);    // position z
+            //landscape.push(0.0f);       // normal x
+            //landscape.push(1.0f);       // normal y
+            //landscape.push(0.0f);       // normal z
+            //landscape.push(xIndex/width);       // tc u PLACEHOLDER
+            //landscape.push((zIndex-1)/height);       // tc v PLACEHOLDER
+            
+            // TRIANGLE 1 VERT 2
+            landscape.push(x - 0.5);    // position x
+            landscape.push(0.0);       // position y
+            landscape.push(z - 0.5);    // position z
+            //landscape.push(0.0f);       // normal x
+            //landscape.push(1.0f);       // normal y
+            //landscape.push(0.0f);       // normal z
+            //landscape.push(xIndex/width);       // tc u PLACEHOLDER
+            //landscape.push(zIndex/height);       // tc v PLACEHOLDER
+            
+            // TRIANGLE 1 VERT 3
+            landscape.push(x + 0.5);    // position x
+            landscape.push(0.0);       // position y
+            landscape.push(z + 0.5);    // position z
+            //landscape.push(0.0f);       // normal x
+            //landscape.push(1.0f);       // normal y
+            //landscape.push(0.0f);       // normal z
+            //landscape.push((xIndex+1)/width);       // tc u PLACEHOLDER
+            //landscape.push((zIndex-1)/height);       // tc v PLACEHOLDER
+            
+            // TRIANGLE 2 VERT 1
+            landscape.push(x + 0.5);    // position x
+            landscape.push(0.0);       // position y
+            landscape.push(z + 0.5);    // position z
+            //landscape.push(0.0f);       // normal x
+            //landscape.push(1.0f);       // normal y
+            //landscape.push(0.0f);       // normal z
+            //landscape.push((xIndex+1)/width);       // tc u PLACEHOLDER
+            //landscape.push((zIndex-1)/height);       // tc v PLACEHOLDER
+            
+            // TRIANGLE 2 VERT 2
+            landscape.push(x - 0.5);    // position x
+            landscape.push(0.0);       // position y
+            landscape.push(z - 0.5);    // position z
+            //landscape.push(0.0f);       // normal x
+            //landscape.push(1.0f);       // normal y
+            //landscape.push(0.0f);       // normal z
+            //landscape.push(xIndex/width);       // tc u PLACEHOLDER
+            //landscape.push(zIndex/height);       // tc v PLACEHOLDER
+            
+            // TRIANGLE 2 VERT 3
+            landscape.push(x + 0.5);    // position x
+            landscape.push(0.0);       // position y
+            landscape.push(z - 0.5);    // position z
+            //landscape.push(0.0f);       // normal x
+            //landscape.push(1.0f);       // normal y
+            //landscape.push(0.0f);       // normal z
+            //landscape.push((xIndex+1)/width);       // tc u PLACEHOLDER
+            //landscape.push(zIndex/height);       // tc v PLACEHOLDER
+        }
+    }
 
-    var boxVertices = new Float32Array ([
-            // Top
-            -1.0, 1.0, -1.0,   0.24, 0.24, 0.24,
-            -1.0, 1.0, 1.0,    0.24, 0.24, 0.24,
-            1.0, 1.0, 1.0,     0.24, 0.24, 0.24,
-            1.0, 1.0, -1.0,    0.24, 0.24, 0.24,
+	var landscapeVBO = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, landscapeVBO);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(landscape), gl.STATIC_DRAW);
 
-            // Left
-            -1.0, 1.0, 1.0,    0.24, 0.24, 0.24,
-            -1.0, -1.0, 1.0,   0.24, 0.24, 0.24,
-            -1.0, -1.0, -1.0,  0.24, 0.24, 0.24,
-            -1.0, 1.0, -1.0,   0.24, 0.24, 0.24,
+	var positionAttribLocation = gl.getAttribLocation(Shader, 'vertPos');
+	gl.vertexAttribPointer(
+		positionAttribLocation, // Attribute location
+		3, // Number of elements per attribute
+		gl.FLOAT, // Type of elements
+		gl.FALSE,
+		3 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		0 // Offset from the beginning of a single vertex to this attribute
+	);
 
-            // Right
-            1.0, 1.0, 1.0,     0.24, 0.24, 0.24,
-            1.0, -1.0, 1.0,    0.24, 0.24, 0.24,
-            1.0, -1.0, -1.0,   0.24, 0.24, 0.24,
-            1.0, 1.0, -1.0,    0.24, 0.24, 0.24,
-
-            // Front
-            1.0, 1.0, 1.0,    0.24, 0.24, 0.24,
-            1.0, -1.0, 1.0,   0.24, 0.24, 0.24,
-            -1.0, -1.0, 1.0,  0.24, 0.24, 0.24,
-            -1.0, 1.0, 1.0,   0.24, 0.24, 0.24,
-
-            // Back
-            1.0, 1.0, -1.0,    0.24, 0.24, 0.24,
-            1.0, -1.0, -1.0,   0.24, 0.24, 0.24,
-            -1.0, -1.0, -1.0,  0.24, 0.24, 0.24,
-            -1.0, 1.0, -1.0,   0.24, 0.24, 0.24,
-
-            // Bottom
-            -1.0, -1.0, -1.0, 0.24, 0.24, 0.24,
-            -1.0, -1.0, 1.0,  0.24, 0.24, 0.24,
-            1.0, -1.0, 1.0,   0.24, 0.24, 0.24, 
-            1.0, -1.0, -1.0, 0.24, 0.24, 0.24
-    ])
-
-    var boxIndices = new Uint16Array ([
-        // top
-        0, 1, 2,
-        0, 2, 3,
-
-        // Left
-        5, 4, 6,
-        6, 4, 7,
-
-        // Right
-        8, 9, 10,
-        8, 10, 11,
-
-        // Front
-        13, 12, 14,
-        15, 14, 12,
-
-        // Back
-        16, 17, 18,
-        16, 18, 19,
-
-        // Bottom
-        21, 20, 22,
-        22, 20, 23
-    ])
-
-    var VBO = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, VBO)
-    gl.bufferData(gl.ARRAY_BUFFER, boxVertices, gl.STATIC_DRAW)
-
-    var EBO = gl.createBuffer()
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO)
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, boxIndices, gl.STATIC_DRAW)
-
-    // upload static shader data
-    var posAttribLoc = gl.getAttribLocation(Shader, 'vertPos')
-    var colAttribLoc = gl.getAttribLocation(Shader, 'vertCol')
-
-    gl.vertexAttribPointer(posAttribLoc, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 0)
-    gl.vertexAttribPointer(colAttribLoc, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT)
-
-    gl.enableVertexAttribArray(posAttribLoc);
-    gl.enableVertexAttribArray(colAttribLoc);
-
+	gl.enableVertexAttribArray(positionAttribLocation);
+    
     gl.useProgram(Shader)
-
+    
     // view transform
     var view  = new Float32Array(16)
     var viewLoc = gl.getUniformLocation(Shader, 'view')
     mat4.lookAt(view, 
-        [0, 0, -5], // position 
+        [0, 2, -5], // position 
         [0, 0, 0],  // forward
         [0, 1, 0]  // up
     );
@@ -163,11 +231,11 @@ function heroAnimation () {
     // LOOP
     var identity = new Float32Array(16)
     var angle = 0;
+    
+    var camZ = -5
 
     mat4.identity(identity)
-    
-  //  gl.polygonMode (gl.FRONT_AND_BACK, gl.LINES)
-    
+
     //window.setTimeout(update,8000);
     function update () {
         // clear
@@ -180,7 +248,7 @@ function heroAnimation () {
         gl.useProgram(Shader)
 
         // model transform
-        angle = performance.now() / 1000 / 6 * 2 * Math.PI;
+        angle = 0 //performance.now() / 1000 / 6 * 2 * Math.PI;
         var model = new Float32Array(16)
         var xRot = new Float32Array(16)
         var zRot = new Float32Array(16)
@@ -206,9 +274,18 @@ function heroAnimation () {
         // pass uniforms to GPU
         gl.uniformMatrix4fv(modelLoc, gl.FALSE, model)
 
+        // move camera forward
+        mat4.lookAt(view, 
+            [0, 2, camZ], // position 
+            [0, 0, 0],  // forward
+            [0, 1, 0]  // up
+        );
+        gl.uniformMatrix4fv(viewLoc, gl.FALSE, view)
+        camZ += 0.01
+        
         // render
-        gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0)
-       // gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 3)
+        //gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0)
+        gl.drawArrays(gl.LINES, 0, landscape.length / 3)
 
         // see you again soon
         requestAnimationFrame(update)
