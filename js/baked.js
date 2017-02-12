2071f7b67fb016a5d2eb40b10f6f3fa571126d78
Ryan Needham <RyanNeedham@Outlook.com>
2017-02-12 00:13:56 +0000

    Tidied up gl code

--- js/baked.js
+++ js/baked.js
@@ -1,57 +1,7 @@
 /* * * * * * * * * * * * * * * * * *
- *  REPLICATE THIS NOISE MESH THING IN WEBGL
+ *  Grid Mesh Animation (baked.js)
  *
- *    int cols, rows;
- *    int scl = 20;
- *    int w = 2000;
- *    int h = 1600;
-
-    float flying = 0;
-
-    float[][] terrain;
-
-    void setup() {
-      size(600, 600, P3D);
-      cols = w / scl;
-      rows = h/ scl;
-      terrain = new float[cols][rows];
-    }
-
-
-    void draw() {
-
-      flying -= 0.1;
-
-      float yoff = flying;
-      for (int y = 0; y < rows; y++) {
-        float xoff = 0;
-        for (int x = 0; x < cols; x++) {
-          terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
-          xoff += 0.2;
-        }
-        yoff += 0.2;
-      }
-
-
-
-      background(23);
-      stroke(64);
-      noFill();
-
-      translate(width/2, height/2+50);
-      rotateX(PI/3);
-      translate(-w/2, -h/2);
-      for (int y = 0; y < rows-1; y++) {
-        beginShape(TRIANGLE_STRIP);
-        for (int x = 0; x < cols; x++) {
-          vertex(x*scl, y*scl, terrain[x][y]);
-          vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
-          //rect(x*scl, y*scl, scl, scl);
-        }
-        endShape();
-      }
-    }
- *
+ *  Ryan Needham
  * * * * * * * * * * * * * * * * * */
 window.onload = heroAnimation;
 
@@ -60,19 +10,40 @@ function heroAnimation () {
     /* * * * * * * * * * * * * * * * * * 
      * CONTEXT
      * * * * * * * * * * * * * * * * * */
-    var canvas = document.getElementById("heroCanvas")
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight * 0.96
-    var gl = canvas.getContext('webgl')
+    var canvas      = document.getElementById("heroCanvas")
+    canvas.width    = window.innerWidth
+    canvas.height   = window.innerHeight * 0.96
+    
+    var gl          = canvas.getContext('webgl')
     gl.clearColor(0.32, 0.32, 0.32, 1.0)
     gl.clear(gl.COLOR_BUFFER_BIT)
     gl.enable(gl.DEPTH_TEST)
     //gl.enable(gl.CULL_FACE)
     //gl.frontFace(gl.CCW)
     //gl.cullFace(gl.BACK)
+    
+    document.addEventListener("resize", function () {
+        canvas.width  = window.innerWidth
+        canvas.height = window.innerHeight * 0.96
+        
+        // projection transform
+        var projection    = new Float32Array(16)
+        var projectionLoc = gl.getUniformLocation(Shader, 'projection')
+        
+        mat4.perspective (
+            projection,
+            glMatrix.toRadian(60), // fov
+            canvas.width / canvas.height, // aspect
+            0.01, // near
+            42 // far
+        );
+
+        gl.uniformMatrix4fv(viewLoc, gl.FALSE, view)
+        gl.uniformMatrix4fv(projectionLoc, gl.FALSE, projection)
+    })
 
     /* * * * * * * * * * * * * * * * * * 
-     * SHADER
+     * SHADERS
      * * * * * * * * * * * * * * * * * */
     var vertexShader = gl.createShader(gl.VERTEX_SHADER)
     gl.shaderSource(vertexShader, [
@@ -85,7 +56,7 @@ function heroAnimation () {
         'float rand(vec2 co) { return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);}',
         'void main (void) {',
             //'fragCol = vertCol;',
-            'float lift = rand(vec2(vertPos.x, vertPos.z));',
+            'float lift = 0.0; //rand(vec2(vertPos.x, vertPos.z));',
             'gl_Position = projection * view * model * vec4(vertPos.x, vertPos.y + lift, vertPos.z, 1.0);',
         '}'
     ].join('\n'))
@@ -107,145 +78,131 @@ function heroAnimation () {
         console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
     }
 
+    // Program
     var Shader = gl.createProgram()
-    gl.attachShader(Shader, vertexShader)
-    gl.attachShader(Shader, fragmentShader)
-    gl.linkProgram(Shader)
+    gl.attachShader (Shader, vertexShader)
+    gl.attachShader (Shader, fragmentShader)
+    gl.linkProgram  (Shader)
 
     /* * * * * * * * * * * * * * * * * * 
-     * MESH
+     * GRID / FLOOR MESH
      * * * * * * * * * * * * * * * * * */
-    var width = 100;
-    var height = 100;
-    var startX = (width / 2) * -1;
-    var endZ = (height / 2) * -1;
+    var width      = 100;
+    var height     = 100;
+    var startX     = (width / 2) * -1;
+    var endZ       = (height / 2) * -1;
+    var xIndex     = 0;
+    var zIndex     = height;
+    var landscape  = [];
+    var step       = 0.5
+    var simplexMachine = new SimplexNoise()
     
-    var xIndex = 0;
-    var zIndex = height;
-    
-    var landscape = [];
-    var step = 0.5
-    
+    // Data
     for (var x = startX; x < width; x += step * 2, xIndex++) {
         for (var z = height; z > endZ; z -= step * 2, zIndex--) {
+            var noise1 = simplexMachine.noise3D(x - step, 0, z + step)
+            var noise2 = simplexMachine.noise3D(x - step, 0, z - step)
+            var noise3 = simplexMachine.noise3D(x + step, 0, z + step)
+            
             // TRIANGLE 1 VERT 1
             landscape.push(x - step);    // position x
-            landscape.push(0.0);       // position y
+            landscape.push(0.0);         // position y
             landscape.push(z + step);    // position z
-            //landscape.push(0.0f);       // normal x
-            //landscape.push(1.0f);       // normal y
-            //landscape.push(0.0f);       // normal z
-            //landscape.push(xIndex/width);       // tc u PLACEHOLDER
-            //landscape.push((zIndex-1)/height);       // tc v PLACEHOLDER
-            
+            //landscape.push(0.0f);      // normal x
+            //landscape.push(1.0f);      // normal y
+            //landscape.push(0.0f);      // normal z
+
             // TRIANGLE 1 VERT 2
             landscape.push(x - step);    // position x
-            landscape.push(0.0);       // position y
+            landscape.push(0.0);         // position y
             landscape.push(z - step);    // position z
-            //landscape.push(0.0f);       // normal x
-            //landscape.push(1.0f);       // normal y
-            //landscape.push(0.0f);       // normal z
-            //landscape.push(xIndex/width);       // tc u PLACEHOLDER
-            //landscape.push(zIndex/height);       // tc v PLACEHOLDER
+            //landscape.push(0.0f);      // normal x
+            //landscape.push(1.0f);      // normal y
+            //landscape.push(0.0f);      // normal z
             
             // TRIANGLE 1 VERT 3
             landscape.push(x + step);    // position x
-            landscape.push(0.0);       // position y
+            landscape.push(0.0);         // position y
             landscape.push(z + step);    // position z
-            //landscape.push(0.0f);       // normal x
-            //landscape.push(1.0f);       // normal y
-            //landscape.push(0.0f);       // normal z
-            //landscape.push((xIndex+1)/width);       // tc u PLACEHOLDER
-            //landscape.push((zIndex-1)/height);       // tc v PLACEHOLDER
+            //landscape.push(0.0f);      // normal x
+            //landscape.push(1.0f);      // normal y
+            //landscape.push(0.0f);      // normal z
             
             // TRIANGLE 2 VERT 1
             landscape.push(x + step);    // position x
-            landscape.push(0.0);       // position y
+            landscape.push(0.0);         // position y
             landscape.push(z + step);    // position z
-            //landscape.push(0.0f);       // normal x
-            //landscape.push(1.0f);       // normal y
-            //landscape.push(0.0f);       // normal z
-            //landscape.push((xIndex+1)/width);       // tc u PLACEHOLDER
-            //landscape.push((zIndex-1)/height);       // tc v PLACEHOLDER
+            //landscape.push(0.0f);      // normal x
+            //landscape.push(1.0f);      // normal y
+            //landscape.push(0.0f);      // normal z
             
             // TRIANGLE 2 VERT 2
             landscape.push(x - step);    // position x
-            landscape.push(0.0);       // position y
+            landscape.push(0.0);         // position y
             landscape.push(z - step);    // position z
-            //landscape.push(0.0f);       // normal x
-            //landscape.push(1.0f);       // normal y
-            //landscape.push(0.0f);       // normal z
-            //landscape.push(xIndex/width);       // tc u PLACEHOLDER
-            //landscape.push(zIndex/height);       // tc v PLACEHOLDER
+            //landscape.push(0.0f);      // normal x
+            //landscape.push(1.0f);      // normal y
+            //landscape.push(0.0f);      // normal z
             
             // TRIANGLE 2 VERT 3
             landscape.push(x + step);    // position x
-            landscape.push(0.0);       // position y
+            landscape.push(0.0);         // position y
             landscape.push(z - step);    // position z
-            //landscape.push(0.0f);       // normal x
-            //landscape.push(1.0f);       // normal y
-            //landscape.push(0.0f);       // normal z
-            //landscape.push((xIndex+1)/width);       // tc u PLACEHOLDER
-            //landscape.push(zIndex/height);       // tc v PLACEHOLDER
+            //landscape.push(0.0f);      // normal x
+            //landscape.push(1.0f);      // normal y
+            //landscape.push(0.0f);      // normal z
         }
     }
 
+    // Buffer Vertices Data
 	var landscapeVBO = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, landscapeVBO);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(landscape), gl.STATIC_DRAW);
 
 	var positionAttribLocation = gl.getAttribLocation(Shader, 'vertPos');
-	gl.vertexAttribPointer(
-		positionAttribLocation, // Attribute location
-		3, // Number of elements per attribute
-		gl.FLOAT, // Type of elements
-		gl.FALSE,
-		3 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		0 // Offset from the beginning of a single vertex to this attribute
-	);
-
+	gl.vertexAttribPointer (positionAttribLocation, 3, gl.FLOAT, gl.FALSE, 3 * Float32Array.BYTES_PER_ELEMENT, 0);
 	gl.enableVertexAttribArray(positionAttribLocation);
-    
+
+    /* * * * * * * * * * * * * * * * * * 
+     * TRANSFORMATION MATRICES
+     * * * * * * * * * * * * * * * * * */
     gl.useProgram(Shader)
     
     // view transform
     var view  = new Float32Array(16)
     var viewLoc = gl.getUniformLocation(Shader, 'view')
     mat4.lookAt(view, 
-        [0, 2, -5], // position 
+        [0, 2, -5],    // position 
         [0, 0, 1000],  // forward
-        [0, 1, 0]  // up
+        [0, 1, 0]      // up
     );
 
     // projection transform
-    var projection = new Float32Array(16)
+    var projection    = new Float32Array(16)
     var projectionLoc = gl.getUniformLocation(Shader, 'projection')
-    mat4.perspective(projection,
-        glMatrix.toRadian(60), // fov
+    mat4.perspective(
+        projection,
+        glMatrix.toRadian(60),        // fov
         canvas.width / canvas.height, // aspect
-        0.01, // near
-        10 // far
+        0.01,                         // near
+        10                            // far
     );
-
 
     gl.uniformMatrix4fv(viewLoc, gl.FALSE, view)
     gl.uniformMatrix4fv(projectionLoc, gl.FALSE, projection)
 
-    // LOOP
+    /* * * * * * * * * * * * * * * * * * 
+     * MAIN LOOP
+     * * * * * * * * * * * * * * * * * */
     var identity = new Float32Array(16)
-    var angle = 0;
-    
-    var camZ = -5
+    var angle    = 0;
+    var camZ     = -5
 
     mat4.identity(identity)
 
-    //window.setTimeout(update,8000);
     function update () {
         // clear
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-        // poll
-       // pollGamepads();
 
         // bind shader
         gl.useProgram(Shader)
@@ -284,7 +241,7 @@ function heroAnimation () {
             [0, 1, 0]  // up
         );
         gl.uniformMatrix4fv(viewLoc, gl.FALSE, view)
-        camZ += 0.1
+        camZ += 0.032
         
         // render
         //gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0)
