041bb9d3b241efc0e2be339897e099b03402f31d
MyForteIsTimeTravel <RyanNeedham@outlook.com>
2017-03-01 12:00:34 +0000

    pushing changes from an update to the live site last week

--- js/main.js
+++ js/main.js
@@ -6,19 +6,28 @@
  *
  *  Ryan Needham
  * * * * * * * * * * * * * * * * * * * * * * * * * * */
+//"use strict";
+document.body.style.opacity = 0.0
+
 const container = document.querySelector('#canvasContainer')
 var   width     = window.innerWidth
-var   height    = window.innerHeight * 0.60
+var   height    = window.innerHeight * 0.95
+
+function betterRand () {
+    var rand = Math.random()
+    if (Math.random() > 0.5) {rand *= -1} 
+    return rand;
+}
 
 // handle window resizing
-window.addEventListener('resize', resizeCallback, false);
+window.addEventListener('resize', resizeCallback, false)
 function resizeCallback () {
     width  = window.innerWidth
-    height = window.innerHeight * 0.60
+    height = window.innerHeight * 0.95
     
     camera.aspect = width/height;
-    camera.updateProjectionMatrix();
-    renderer.setSize(width, height);
+    camera.updateProjectionMatrix()
+    renderer.setSize(width, height)
 }
 
 /* * * * * * * * * * * * * *
@@ -39,6 +48,7 @@ const camera   = new THREE.PerspectiveCamera(
 
 const scene      = new THREE.Scene()
 scene.background = new THREE.Color(0x202020)
+//scene.background = new THREE.Color(0xFFFFFF)
 scene.add(camera)
 
 camera.position.z = 5
@@ -53,14 +63,24 @@ container.appendChild(renderer.domElement)
 /* * * * * * * * * * * * * *
  * Lighting
  * * * * * * * * * * * * * */
-const pointLight = new THREE.PointLight(0xFFFFFF)
+const pointLight      = new THREE.PointLight(0xFFFFFF)
 pointLight.position.x = 0
 pointLight.position.y = 0
 pointLight.position.z = 600
 pointLight.rotation   = 20 * (Math.PI / 180)
 pointLight.castShadow = true
-pointLight.power = 2
+pointLight.power      = 2
 scene.add(pointLight)
+
+const pointLight2      = new THREE.PointLight(0xFFFFFF)
+pointLight2.position.x = 0
+pointLight2.position.y = 0
+pointLight2.position.z = -600
+pointLight2.rotation   = 20 * (Math.PI / 180)
+pointLight2.castShadow = true
+pointLight2.power      = 2
+scene.add(pointLight2)
+
 
 const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.2)
 scene.add(ambientLight)
@@ -69,38 +89,30 @@ scene.add(ambientLight)
  * World Objects
  * * * * * * * * * * * * * * * */
 var loader = new THREE.JSONLoader();
+
 /** 
  *  This cannot be allowed to run asynchronously with the rest of the program
  *  or the render calls will throw null errors while this gets dragged off
  *  disk
  */
 loader.load('assets/asteroid.json', function (geometry) {
-    var material = new THREE.MeshLambertMaterial({color:0xc19170});
-    var mesh     = new THREE.Mesh( geometry, material );
-
-    mesh.position.x = 0;
-    mesh.position.y = 0;
-    mesh.position.z = 0;
-    
+    var material       = new THREE.MeshLambertMaterial({color:0xc19170});
+    var mesh           = new THREE.Mesh( geometry, material );
+    mesh.position.x    = 0;
+    mesh.position.y    = 0;
+    mesh.position.z    = 0;
     mesh.receiveShadow = true
     mesh.castShadow    = true
-        
     scene.add(mesh);
 
-    var starCount = 1000
-    var stars = new Array()
+    var starCount  = 1000
+    var stars      = new Array()
     var starSpeeds = new Array()
-
-    function betterRand () {
-        var rand = Math.random()
-        if (Math.random() > 0.5) {rand *= -1} 
-        return rand;
-    }
 
     // one way
     for (var i = 0; i < starCount; i++) {
         const obj = new THREE.Mesh(
-            new THREE.SphereGeometry( 0.2, 4, 4 ),               // Vertex Shader
+            new THREE.SphereGeometry( 0.2, 4, 4 ),            // Vertex Shader
             new THREE.MeshBasicMaterial({color: 0xDDDDDD})    // Fragment Shader
         );
 
@@ -128,7 +140,7 @@ loader.load('assets/asteroid.json', function (geometry) {
         }
         scene.add(obj)
     }
-
+    
     /* * * * * * * * * * * * * * * *
      * ON UPDATE
      * * * * * * * * * * * * * * * */
@@ -138,27 +150,27 @@ loader.load('assets/asteroid.json', function (geometry) {
     var radius = 10
     var spin   = 4
 
-    function update () {
+    function update () {    
         if (!paused) {
             updateTick()
             updateInput()
+            
+            // fade in
+            document.body.style.opacity = 1.0;
 
             // Pan the camera around the asteroid
             camera.position.x = (Math.sin((tick + (mouseX*0.005))/spin) * radius)
             camera.position.z = (Math.cos((tick + (mouseY*0.005))/spin) * radius)
             camera.lookAt(scene.position)
-
+            
             for (var i = 0; i < starCount; i++) {
-                if (stars[i].position.y > -200) {
-                   stars[i].position.y -= starSpeeds[i];
-                }
-                else {
-                    stars[i].position.y = 180
-                }
+                if (stars[i].position.y > -200) { stars[i].position.y -= starSpeeds[i] }
+                else                            { stars[i].position.y = 180 }
             }
 
+
             if (Math.random() > 0.5) { mesh.position.x += Math.random() * 0.001 }
-            else { mesh.position.x -= Math.random() * 0.001 }
+            else                     { mesh.position.x -= Math.random() * 0.001 }
             mesh.rotation.z += 0.01;
             mesh.rotation.y += 0.01;   
 
@@ -173,14 +185,11 @@ loader.load('assets/asteroid.json', function (geometry) {
     // guard against unsafe integer values
     function updateTick () {
         switch (tick == Number.MAX_SAFE_INTEGER) {
-            case true:  tick = 0; break
-            case false: tick += 0.01;   break
+            case true:  tick = 0;     break
+            case false: tick += 0.01; break
         }
     }
-
+    
     // Entry Point
     requestAnimationFrame(update);
-    
 }); 
-
-
